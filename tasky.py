dde5b682777ba0bc86887ba10275716c76d4d408
govind somalal <govindsomalal7@gmail.com>
2018-12-28 16:12:10 +0530

    initial commit - a task manager that makes sense for me
    Right now this is just a POC that I whipped up in a lunch break, that
    said this idea has merit. The idea is to have tasks searchable
    by arbitrary tags. I can choose to use the tags in a disciplined yet
    somewhat flexible way. The task manager itself acts like a pile you
    shove your tasks into with tags. When you want to do something you use
    the taskmanager to index this big pile with tags giving you a list of
    things appropriate for that context.
    
    Here ae a couple tihngs I have in mind.
    "tasky list any quick mall" - find anything that is either quick or can
    be done at a mall (or both)
    
    "tasky list only easy table" - find anything that can be done at the
    table with low mental effort
    
    "tasky list only  easy home due today" - find any easy thing that can be
    done at home and which is due today.

---
+++ tasky.py
@@ -0,0 +1,41 @@
+class task(object):
+    def __init__(self, **kwargs):
+
+        self.title = kwargs['title']
+        self.tags  = set() if len(kwargs) == 1 else set(kwargs['tags'])
+        self.due   = kwargs['due']
+        self.done  = False
+    
+    def __str__(self):
+        status = 'done' if self.done is True else 'todo'
+        return f'{self.title.ljust(20)} {self.due.ljust(20)} {status}'
+        
+class tasklist(object):
+    def __init__(self, tasks):
+        self.tasks = tasks
+    def list_for_tags(self, taglist):
+        result = []
+        for task in self.tasks:
+            if len(task.tags.intersection(taglist)) > 0 :
+                result.append(task)
+        return result
+
+
+##unit test
+def printmenu():
+    return int(input('1.View 2.Add 3.Search \n\n'))
+task1 = task(title = 'buy milk', tags= {'mall','quick'},due='by next thursday') 
+task2 = task(title = 'clean room', tags= {'house','quick'},due='today') 
+main_task_list = tasklist(tasks = [task1, task2])
+while(True):
+    choice = printmenu()
+    if choice not in {1,2,3}:
+        break
+    else:
+        if choice == 1:
+            for t in main_task_list.tasks:
+                print(t)
+            continue
+        if choice == 3:
+            query = set(input('enter a thing').split())
+            print(*main_task_list.list_for_tags(query), sep='\n\n')
