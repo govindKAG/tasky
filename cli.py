a9de1d370e2cc045f8867841781e50b6140cae59
govind somalal <govindsomalal7@gmail.com>
2018-12-28 17:56:29 +0530

    added smart date parsing

---
+++ cli.py
@@ -0,0 +1,21 @@
+import dateparser as dp
+import sys
+
+DUE = 'due'
+ANY = 'any'
+ONLY = 'only'
+LIST = 'list'
+
+arguments = sys.argv[1:]
+
+print(arguments)
+
+if DUE in arguments:
+
+    due_index = arguments.index(DUE)
+    due_date = ' '.join(arguments[due_index + 1:])
+    arguments = arguments[:due_index]
+
+print(due_date)
+print(dp.parse(due_date))
+print(f'arguments after removal {arguments}')
