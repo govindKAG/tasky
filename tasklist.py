40be432501faef4d8c33284f021a1f8a4454f3d2
govind somalal <govindsomalal7@gmail.com>
2019-01-03 14:24:45 +0530

    implemented some new features and refactored some code
    refactored prettytable logic out to util
    refactored sqlification of list of strings to util
    added list any

--- tasklist.py
+++ tasklist.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import sqlite3
+from util import prettyprint
+from util import list_to_sql_params
 
 from tabulate import tabulate
 pd.set_option('display.max_columns', None)
 
+DB = "taskyData.db"
+TABLE = "tasky"
 class taskList(object):
     def __init__(self):
-        self.connection = sqlite3.connect('taskyData.db')
+        self.connection = sqlite3.connect(DB)
+
     def add(self, title, due, date_added, date_completed, tag1=None,tag2=None,tag3=None,tag4=None,tag5=None,tag6=None):
         values = {'title':title, 'due':due,'date_added':date_added,'date_completed':date_completed,'tag1':tag1,'tag2':tag2,'tag3':tag3, 'tag4':tag4, 'tag5':tag5, 'tag6':tag6}
         columns = ', '.join(values.keys())
@@ -14,15 +19,40 @@ class taskList(object):
         sql = 'INSERT INTO {table} ({}) VALUES ({})'.format(columns, placeholders, table = 'tasky')
         with self.connection as cur:
             cur.execute(sql, list(values.values()))
+
     def view_all(self):
-        df = pd.read_sql_query("SELECT * FROM tasky", self.connection)
-        print(tabulate(df, headers='keys', tablefmt='psql'))
+        df = pd.read_sql_query(f"SELECT * FROM {TABLE}", self.connection)
+        prettyprint(df)
+
+    def list_all_tags(self, taglist=None):
+        taglist = list_to_sql_params(taglist)
+
+        df = pd.read_sql_query( f'''SELECT * FROM {TABLE} WHERE  
+                tag1 IN {taglist} OR
+                tag2 IN {taglist} OR
+                tag3 IN {taglist} OR
+                tag4 IN {taglist} OR
+                tag5 IN {taglist} OR
+                tag6 IN {taglist};
+                ''', self.connection)
+
+        prettyprint(df)
+    def list_only_tags(self, taglist=None):
+        df = pd.read_sql_query(f"SELECT * FROM {TABLE}", self.connection)
+        prettyprint(df.loc[df['tag1'].isin(taglist) & ]df.loc[df['tag1'].isin(taglist) & df.loc[df['tag1'].isin(taglist) & df.loc[df['tag1'].isin(taglist) & df.loc[df['tag1'].isin(taglist) & df.loc[df['tag1'].isin(taglist) & )
 
 
+#unit test
 mainlist = taskList()
 mainlist.view_all()
-mainlist.add(title = 'write project report', due='1/1/19', date_added='30/12/18', date_completed= '12/12/12/', tag1 = 'hard', tag2 = 'time consuming', tag3 = 'laptop')
-mainlist.view_all()
+mainlist.list_all_tags(taglist=['easy', 'hard'])
+mainlist.list_all_tags(taglist=['hard'])
+mainlist.list_all_tags(taglist=['mall'])
+
+#mainlist.add(title = 'write project report', due='1/1/19', date_added='30/12/18', date_completed= '12/12/12/', tag1 = 'hard', tag2 = 'time consuming', tag3 = 'laptop')
+#mainlist.view_all()
+#mainlist.add(title = 'buy razors', due='1/1/19', date_added='30/12/18', date_completed= '12/12/12/', tag1 = 'easy', tag2 = 'supermarket')
+#mainlist.view_all()
 ##con = sqlite3.connect('taskyData.db')
 #
 ##values = {'title':"buy some milk", 'due':'1/12/19','date_added':'1/1/19','date_completed':"2/1/19",'tag1':'easy','tag2':'mall','tag3':'urgent'}
