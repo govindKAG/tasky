3ab4542cc3baa1be03228725db332cc58d5bf0fb
govind somalal <govindsomalal7@gmail.com>
2019-01-08 11:37:47 +0530

    added util function for natural language date parsing

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
 import pandas as pd 
 from  tabulate import tabulate
+import datetime as dt
+import dateparser as dp
+from timefhuman import timefhuman as nlp
+
+DATE_FORMAT = '%d/%m/%Y'
 
 def prettyprint(df):
     print(tabulate(df, headers='keys', tablefmt='psql'))
@@ -10,3 +15,21 @@ def list_to_sql_params(taglist):
     if len(taglist) == 1:
         taglist = "('" + taglist[0] + "')"
     return taglist
+
+def format_date(date):
+    return date.strftime(DATE_FORMAT)
+
+def parse_date(description):
+    parsed_date = None
+    try:
+        parsed_date =  nlp(description)
+    except:
+        print('hit fallback')
+        parse_date = dp.parse(description)
+    return parsed_date
+        
+
+print(parse_date('after 2 days'))
+print(parse_date('tomorrow'))
+print(parse_date('in 4 days'))
+print(parse_date('december 10'))
