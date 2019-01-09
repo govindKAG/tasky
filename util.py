a1c0851463ad163723f0ea26673d9910c77253c2
govind somalal <govindsomalal7@gmail.com>
2019-01-09 13:27:54 +0530

    added method to filter a df by the due date

--- util.py
+++ util.py
@@ -1,13 +1,15 @@
-import pandas as pd 
-from  tabulate import tabulate
+import pandas as pd
+from tabulate import tabulate
 import datetime as dt
 import dateparser as dp
 from timefhuman import timefhuman as nlp
 
 DATE_FORMAT = '%d/%m/%Y'
 
+
 def prettyprint(df):
     print(tabulate(df, headers='keys', tablefmt='psql'))
+
 
 def list_to_sql_params(taglist):
     og = taglist
@@ -16,19 +18,26 @@ def list_to_sql_params(taglist):
         taglist = "('" + taglist[0] + "')"
     return taglist
 
+
 def format_date(date):
     return date.strftime(DATE_FORMAT)
+
 
 def parse_date(description):
     parsed_date = None
     try:
-        parsed_date =  nlp(description)
+        parsed_date = nlp(description)
     except:
         print('hit fallback')
         parse_date = dp.parse(description)
     return parsed_date
-        
 
+def filter_due_date(df, due):
+    df['is_due'] = df['due'].apply(lambda x: parse_date(x) <=  parse_date(due))
+    return df[df['is_due'] == True]
+
+
+#unit test
 print(parse_date('after 2 days'))
 print(parse_date('tomorrow'))
 print(parse_date('in 4 days'))
